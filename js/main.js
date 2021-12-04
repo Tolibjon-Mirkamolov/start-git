1bc4f6eda6089c475fb5cd11754bb71b4808622e
Tolibjon <pubgdev150@gmail.com>
2021-12-04 12:50:59 +0500

    Fikir va takliflar uchun

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// $('.actiors__slider-img').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.actiors__slider-txt'
+// });
+// $('.actiors__slider-txt').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     asNavFor: '.actiors__slider-img',
+//     dots: true,
+//     centerMode: true,
+//     focusOnSelect: true
+// });
